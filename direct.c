ada1db595fb76a3aa036e94cbe2245d4f4555a5b
thealgebraist2025 <bruun.anders.2025@gmail.com>
2025-10-09 22:15:58 +0200

    Update direct.c

--- direct.c
+++ direct.c
@@ -7,7 +7,7 @@
 // --- STB Image Configuration ---
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb_read.h"
 #include "stb_image_write.h"
 
 #define M_PI 3.14159265358979323846
