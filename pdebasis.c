d6e8c8eed2ecadbddaa79ce374eb8bf94692db5c
thealgebraist2025 <bruun.anders.2025@gmail.com>
2025-10-13 23:20:57 +0200

    Update pdebasis.c

--- pdebasis.c
+++ pdebasis.c
@@ -17,7 +17,7 @@
 #define N_OUTPUT 10            // 3 Class + 3 Circle Params + 4 Rectangle Params
 
 // **Training Parameters**
-#define NUM_IMAGES 3000        
+#define NUM_IMAGES 1000        
 #define TRAINING_TIME_LIMIT 160.0 // Stop training after 60 seconds
 #define BATCH_SIZE 32          
 #define REPORT_FREQ 500             
